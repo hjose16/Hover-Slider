f1f695c2d48f91962c49191f1988f30e64217c43
hemax16 <68761852+hemax16@users.noreply.github.com>
2021-05-27 16:46:45 -0400

    Agregamos archivos del proyecto

---
+++ codigo.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+    const imgLigthbox = document.getElementById("enlace");
+    M.Materialbox.init(imgLigthbox, {
+        inDuration: 500,
+        outDuration: 500
+    });
+});
